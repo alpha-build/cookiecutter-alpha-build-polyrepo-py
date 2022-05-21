ce7a068ada22c982b5d96dd219a52ae10b834d5f
Cristian Matache <cristianmatache@hotmail.com>
2022-05-21 02:00:26 +0100

    setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 LIB_ROOT = Path(__file__).parent
 REQS_FILE = LIB_ROOT / 'requirements.txt'
@@ -10,12 +10,12 @@ README_FILE = LIB_ROOT / 'README.md'
 def main() -> None:
     setup(
         name='py_utils',
-        version='0.1',
-        packages=['{{cookiecutter.repo_name}}'],
-        python_requires='>=3.8',
+        version='0.0.1',
+        packages=find_packages(exclude=['tests*']),
+        python_requires='>=3.7',
         install_requires=REQS_FILE.read_text(),
         zip_safe=False,  # for mypy
-        package_data={'': ['py.typed']},  # expose types to users  # TODO
+        package_data={'{self.lib_name}': ['py.typed']},  # expose types to users  # TODO
         author='{{cookiecutter.author}}',
         author_email='{{cookiecutter.email}}',
         description='',
