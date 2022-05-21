ee76d3f1fe7e6d85d9bb8890fb14f546a285eae4
Cristian Matache <cristianmatache@hotmail.com>
2022-05-21 02:27:13 +0100

    setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -9,13 +9,13 @@ README_FILE = LIB_ROOT / 'README.md'
 
 def main() -> None:
     setup(
-        name='py_utils',
+        name='{{cookiecutter.project_name}}',
         version='0.0.1',
         packages=find_packages(exclude=['tests*']),
         python_requires='>=3.7',
-        install_requires=REQS_FILE.read_text(),
+        install_requires=REQS_FILE.read_text(encoding='utf-8'),
         zip_safe=False,  # for mypy
-        package_data={'{self.lib_name}': ['py.typed']},  # expose types to users  # TODO
+        package_data={'{self.lib_name}': ['py.typed']},  # expose types to users
         author='{{cookiecutter.author_name}}',
         author_email='{{cookiecutter.author_email}}',
         description='',
